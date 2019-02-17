a6bdeb386cd5cb67bbf0302bf75509dc23f1d49c
0xflotus <0xflotus@gmail.com>
2019-02-17 23:58:17 +0100

    added examples and epilog

--- terra.js
+++ terra.js
@@ -27,7 +27,10 @@ const yargs = require("yargs")
   .help("h")
   .alias("h", "help")
   .alias("V", "version")
-  .usage("Usage: $0 -f GBP -t EUR -a 23")
+  .usage("Usage: $0 -f [from] -t [to] -a [amount]")
+  .example("$0 -f USD -t EUR -a 24")
+  .example("$0 -t JPY")
+  .epilog("Published under MIT LICENSE by 0xflotus 2019")
   .parse();
 
 const AMOUNT = yargs.amount || yargs._[1];
