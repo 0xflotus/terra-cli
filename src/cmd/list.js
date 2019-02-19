36cfc6cb9cd09a4ece02ae75c1cb39bd4afff902
0xflotus <0xflotus@gmail.com>
2019-02-19 04:01:11 +0100

    refactorings

--- src/cmd/list.js
+++ src/cmd/list.js
@@ -2,10 +2,9 @@ exports.command = ["list", "ls"];
 
 exports.describe = "List supported ISO 4217 Codes";
 
-exports.builder = ({ argv: { currencies } }) => {
+exports.builder = ({ argv: { currencies } }) =>
   Object.keys(currencies).forEach((code, index) =>
     console.log("%d. \t%s - %s", index + 1, code, currencies[code])
   );
-};
 
 exports.handler = _ => {};
